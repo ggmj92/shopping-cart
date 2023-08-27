d01da30f202c3cf41313df6095917b46782f0b01
Gabriel Garcia-Miro Jeri <ggmj@Gabriels-MacBook-Pro-2.local>
2023-08-27 09:12:53 -0400

    recovered

---
+++ js/main.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const chooseProducts = document.querySelector('#chooseProducts')
+    const productSpace = document.querySelector('#productSpace')
+    const emptyCart = document.querySelector('#emptyCart')
+    const totalPrice = document.querySelector('#totalPrice')
+    const btnCheckout = document.querySelector('#btnCheckout')
+    const btnClear = document.querySelector('#btnClear')
+    let subtotalSum = 0
+
+    const baseURL = 'https://dummyjson.com/products'
+
+    const fragment =  document.createDocumentFragment()
+
+}) // LOAD
